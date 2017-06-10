0aad8bddc2e309ceb37ac908c11056146d13a0ab
Dmitry Shadrin <dmitryshadrin@mbp-dmitry.dlink>
2017-06-10 21:16:35 +0300

    Fix warnings

---
+++ Fast Forecast/DSAdviceAndBackgroundCast.h
@@ -0,0 +1,13 @@
+//
+//  DSAdviceAndBackgroundCast.h
+//  Fast Forecast
+//
+//  Created by Dmitry Shadrin on 10.06.17.
+//  Copyright © 2017 Dmitry Shadrin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSAdviceAndBackgroundCast : NSObject
+
+@end
---
+++ Fast Forecast/DSAdviceAndBackgroundCast.m
@@ -0,0 +1,13 @@
+//
+//  DSAdviceAndBackgroundCast.m
+//  Fast Forecast
+//
+//  Created by Dmitry Shadrin on 10.06.17.
+//  Copyright © 2017 Dmitry Shadrin. All rights reserved.
+//
+
+#import "DSAdviceAndBackgroundCast.h"
+
+@implementation DSAdviceAndBackgroundCast
+
+@end
